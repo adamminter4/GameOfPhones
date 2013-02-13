8400b6b75af4d9cf0ad0b13241f3b40621e403fb
Caleb Gutshall <cgutshal@gmail.com>
2013-02-13 00:30:38 -0500

    Oauth is working with Foursquare.

---
+++ GameOfPhones/src/com/example/gameofphones/LoginToFoursquare.java
@@ -0,0 +1,26 @@
+package com.example.gameofphones;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class LoginToFoursquare extends Activity {
+
+	 @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_login_to_foursquare);
+	        
+	        Button btn = (Button)findViewById(R.id.button);
+	        btn.setOnClickListener(new OnClickListener() {
+	            public void onClick(View v) {
+	                Intent intent = new Intent(LoginToFoursquare.this, FoursquareOauthWebVeiw.class);
+	                startActivity(intent);
+	            }
+	        });
+	    }
+
+}
